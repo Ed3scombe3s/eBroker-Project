087f7877b6c62265908f4c12461dad5412e2e36c
Ed3scombe3s <118176935+Ed3scombe3s@users.noreply.github.com>
2022-11-23 21:23:26 +0100

    Add files via upload

--- Final Project/All_Functions.py
+++ Final Project/All_Functions.py
@@ -5,6 +5,12 @@ import bcrypt
 import pandas as pd
 import yfinance as yf
 import mplfinance as mpf
+from prettytable import PrettyTable
+
+
+
+
+
 
 
 ############### User Functions ############################
@@ -15,59 +21,162 @@ import mplfinance as mpf
 
 def Current_user(user): # Current user
     global current_user
-    current_user = user
+    global name
+    with open("users.txt", "r+") as file:
+        for line in file:
+            login_info = line.split()
+            name = login_info[0].capitalize()
+            current_user = login_info[3]
 
 def clear(): # Clear the screen
     os.system('cls' if os.name == 'nt' else 'clear')
 
+
+
+class add_New_user():
+    def __init__(self, name, age, email, user, password):
+        self.name = name
+        self.age = age
+        self.email = email
+        self.user = user 
+        self.password = password
+    
+    def add_user_to_db(self):
+        with open("users.txt", "a") as file:
+            file.write(self.name + " " + self.age + " " + self.email + " " + self.user + " " + self.password + "\n")
+        with open('users_portfolio.csv', 'a', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow([self.user, 10_000, "No", "N/A", 0, 0, "N/A"])
+    
+
+
 def creat_user(): # Create a new user
     clear()
     print("Create a new user")
-    user = input("Enter your user name: ")
-    user = user.capitalize()
+    name = input("Enter your name: ")
+    age = input("Enter your age: ")
+    email = input("Enter your email: ")
+    user = input("Enter your username: ")
     if user == "":
         return main()
     with open("users.txt", "r+") as file:
         for line in file:
             login_info = line.split()
-            if login_info[0] == user:
-                print("User already exists")
+            if login_info[3] == user:
+                print("Username already exists")
+                time.sleep(2)
+                return main()
+            if login_info[2] == email:
+                print("Email already in use")
                 time.sleep(2)
                 return main()
     password = input("Enter your password: ")
     password = password.encode('utf-8')
     password = bcrypt.hashpw(password, bcrypt.gensalt())
-    with open("users.txt", "a") as file:
-        file.write(user + " " + str(password) + "\n")
+    new_user = add_New_user(name, age, email, user, str(password))
+    new_user.add_user_to_db()
     print("User created successfully!")
-    add_user(user)
     time.sleep(1)
     main()
+
+
 
 def login(): # Login to the system
     clear()
     print("Login to the system")
-    user = input("Enter your user name: ")
-    user = user.capitalize()
+    user = input("Enter your username: ")
     password = input("Enter your password: ")
     with open("users.txt", "r") as file:
         for line in file:
             login_info = line.split()
-            hashed = login_info[1].strip('b')
+            hashed = login_info[4].strip('b')
             hashed = hashed.replace("'", "")
             hashed = hashed.encode('utf-8')
-            if user == login_info[0] and bcrypt.checkpw(password.encode(), hashed):
+            if user == login_info[3] and bcrypt.checkpw(password.encode(), hashed):
                 print("Login successful!")
                 Current_user(user)
                 return True
         return False
+
+
+def Convert(tup, di):
+    for a, b in tup:
+        di.setdefault(a, []).append(b)
+    return di
+        
+def sort_by_price(mydict, choice):
+    d_items = list(mydict.items())
+    for i in range(len(d_items)):
+        m_value = d_items[i][1]
+        for j in range(i+1, len(d_items)):
+            if choice == "1":
+                if d_items[j][1] < m_value:
+                    m_value = d_items[j][1]
+                    d_items[i], d_items[j] = d_items[j], d_items[i]
+            else:
+                if d_items[j][1] > m_value:
+                    m_value = d_items[j][1]
+                    d_items[i], d_items[j] = d_items[j], d_items[i]
+    return d_items
+
+
+def ticker_info():
+    clear()
+ 
+    print("Please wait why we load the data...")
+
+    ticker_live_dict = {}
+    tickers = ["meta", "aapl", "msft", "tsla", "amzn", "goog", "nflx", "nvda", "pypl", "goog", "^spx"]
+    for ticker in tickers:
+        price = round(get_live_price(ticker),2)
+        price = float(price)
+        ticker_live_dict[ticker] = price
+    clear()
+    print("Sort tickers by price")
+    print("1. Ascending")
+    print("2. Descending")
+
+    choice = input("Enter your choice: ")
+    if choice == "1":
+        a = sort_by_price(ticker_live_dict, choice)
+        order = "Ascending"
+    elif choice == "2":
+        a = sort_by_price(ticker_live_dict, choice)
+        order = "Descending"
+    elif choice == "":
+        main()
+    else:
+        clear()
+        print("Invalid choice!")
+        time.sleep(1)
+        ticker_info()
+    ticker_live_dict = {}
+    Convert(a, ticker_live_dict)
+    clear()
+    t = PrettyTable(['Ticker', 'Price'])
+    for x, y in ticker_live_dict.items():
+        t.title = order + " order"
+        y = str(y)
+        t.add_row([x, y.replace("[", "").replace("]", "")])
+    print(t)
+    input("Press enter to continue:")
+    try:
+        if Current_user:
+            LoggedIn()
+    except:
+        main()
+
+
+
+
 
 def main(): # Main function
     clear()
     print("Welcome to the eTrader Platform!")
     print("1. Create a new user")
     print("2. Login")
-    print("3. Exit")
+    print("3. See the top 10 stocks")
+    print("4. Exit")
     choice = input("Enter your choice: ")
     if choice == "1":
         creat_user()
@@ -79,6 +188,8 @@ def main(): # Main function
             time.sleep(2)
             main()
     elif choice == "3":
+        ticker_info()
+    elif choice == "4":
         exit()
     else:
         clear()
@@ -89,13 +200,16 @@ def main(): # Main function
 
 def LoggedIn(): # Logged in menu
     clear()
-    print(f'Hello, {current_user}!')
+    print(f'Hello, {name}!')
     print("1. Go to Portfolio")
-    print("2. Logout")
+    print("2. See the top 10 stocks")
+    print("3. Logout")
     choice = input("Enter your choice: ")
     if choice == "1":
         Portfolio(current_user)
     elif choice == "2":
+        ticker_info()
+    elif choice == "3":
         main()
     else:
         clear()
@@ -115,22 +229,17 @@ def LoggedIn(): # Logged in menu
 
 
 
-
-def add_user(user):
-    with open('users_portfolio.csv', 'a', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow([user, 10_000, "No", "N/A", 0, 0, "N/A"])
-
 def Balance(user):
     with open('users_portfolio.csv', 'r') as file:
         reader = csv.reader(file)
         for row in reader:
             if row[0] == user:
                 row[1] = float(row[1])
-                print(f"Balance is: {row[1]:.2f}")
+                print(f"Balance is: {row[1]:0,.2f}$")
                 global balance
                 balance = row[1]
                 balance = float(balance)
+
                 
 
 def Opened_Position(user):
@@ -242,10 +351,10 @@ def Close_Position(user):
                 close_price = get_live_price(ticker)
                 close_price = float(close_price)
                 invested = shares * open_price
+                
                 if type == "long":
                     profit = (close_price - open_price) * shares
                     new_balance = balance + invested + profit
-                    update_user_portfolio(user, new_balance, "No", "N/A", 0, 0, "N/A") 
 
                 elif type == "short":
                     profit = (open_price - close_price) * shares
@@ -255,17 +364,23 @@ def Close_Position(user):
                     print("You don't have any opened position")
                     time.sleep(2)
                     Portfolio(user)
-
-                update_user_portfolio(user, new_balance, "No", "N/A", 0, 0, "N/A") 
-                print(f"You opened a {type} position on {ticker} of {shares:.4f} shares at a price of {open_price} for {invested:.2f} dollars")
-                print(f"You sold your {shares:.4f} shares of {ticker} at {close_price} for a total of {invested + profit:.2f} dollars")
-                print(f"Profit: {profit:.2f}")
-                input("\nPress enter to continue")
-                Portfolio(user)
-
+                print("Are you sure you want to close your position? (y/n): ")
+                print(f"{ticker}'s Current price: {close_price}")
+                print(f"Current PnL is: {profit:.2f}")
+                sure = input(":")
+                if sure == "y":
+                    update_user_portfolio(user, new_balance, "No", "N/A", 0, 0, "N/A") 
+                    print(f"You opened a {type} position on {ticker} of {shares:.4f} shares at a price of {open_price} for {invested:.2f} dollars")
+                    print(f"You sold your {shares:.4f} shares of {ticker} at {close_price} for a total of {invested + profit:.2f} dollars")
+                    print(f"Profit: {profit:.2f}")
+                    input("\nPress enter to continue")
+                    Portfolio(user)
+                else:
+                    Portfolio(user)
+                
 def Portfolio(user):
     clear()
-    print(f"{user}'s Portfolio")
+    print(f"{name}'s Portfolio")
 
     #check users balance
     Balance(user)
@@ -285,6 +400,7 @@ def Portfolio(user):
 
     option = input("Enter your option: ")
     if option == "1":
+        Open_Position(user)
         if postion_opened == True:
             print("You already have an opened position!")
             time.sleep(1)
@@ -373,7 +489,7 @@ def get_market_data():
 
 def plot_market_data(data , ticker):
     #declare figure
-    mpf.plot(data, type='candle', style='yahoo', title=ticker, ylabel='Stock Price (USD per Shares)', volume=True, mav=(2,5))
+    mpf.plot(data, type='candle', style='yahoo', title=ticker, ylabel='Stock Price (USD per Shares)', volume=True, mav=(2,5,200))
 
 
 
